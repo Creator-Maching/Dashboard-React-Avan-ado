e96edef1b0386073f06ff47df409e0df550de9ca
Pedro Dias <ordepmeta@gmail.com>
2025-11-25 15:52:04 -0300

    deploy

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: "/Dashboard-React-Avan-ado/",
   plugins: [react()],
-  // Se você quiser publicar no GitHub Pages, pode adicionar a base:
-  // base: '/NOME_DO_REPOSITORIO/',
 });
